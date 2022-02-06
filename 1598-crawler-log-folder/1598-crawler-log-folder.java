1b39028e7775bf6f8cbf8fd5623772b9dc5e99d4
Seonjun Moon <70519480+thelovemsg@users.noreply.github.com>
2022-02-06 17:01:36 +0900

    Time: 2 ms (40.75%), Space: 41.9 MB (13.44%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.java
+++ 1598-crawler-log-folder/1598-crawler-log-folder.java
@@ -5,20 +5,16 @@ class Solution {
            
             switch(temp){
                 case "./":
-                    System.out.println("그대로");
                     break;
                 case "../":
-                    System.out.println("뒤로");
                     result--;
                     break;
                 default:
-                    System.out.println("안으로");
                     result++;
                     break;
             }
             if(result <=0)
                 result = 0;
-            System.out.println("-+--------------");
         }
         
         
