610fef10dc5e669547b72ca6400d5afb1999acbf
Seonjun Moon <70519480+thelovemsg@users.noreply.github.com>
2022-02-20 15:12:22 +0900

    Time: 8 ms (56.63%), Space: 46.5 MB (16.85%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.java
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.java
@@ -3,7 +3,6 @@ class Solution {
         Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
         int[] curr = new int[]{-1, -1};
         int res = 0;
-        System.out.println("size of curr : " + curr.length);
         for(int[] elem : intervals){
             if(elem[0] > curr[0] && elem[1] > curr[1]){
                 curr[0] = elem[0];
