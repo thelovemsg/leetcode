844a4ae5d9415cc248f9f860618462b5bad95fbd
Seonjun Moon <70519480+thelovemsg@users.noreply.github.com>
2022-08-27 00:26:09 +0900

    Time: 3 ms (44.67%), Space: 40.4 MB (84.67%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.java
+++ 869-reordered-power-of-2/869-reordered-power-of-2.java
@@ -1,18 +1,21 @@
 class Solution {
-    public boolean reorderedPowerOf2(int n) {
-        char[] number = sortedDigits(n);
-
-        for (int i = 0; i < 30; ++i) {
-            char[] powerOfTwo = sortedDigits(1 << i);
-            if (Arrays.equals(number, powerOfTwo))
-                return true;
+   public boolean reorderedPowerOf2(int n) {
+        Set<Long> two = new HashSet<>();
+        for (int i = 1; i <= (int)1e9; i <<= 1){
+            two.add(transform(i));
         }
-        return false;
+
+        return two.contains(transform(n));
     }
 
-    private char[] sortedDigits(int n) {
-        char[] digits = String.valueOf(n).toCharArray();
-        Arrays.sort(digits);
-        return digits;
+    private long transform(int n){
+        long sum = 0;
+        while(n > 0){
+            int d = n % 10;
+            sum += 1L << (d*3);
+            n /= 10;
+        }
+
+        return sum;
     }
 }
