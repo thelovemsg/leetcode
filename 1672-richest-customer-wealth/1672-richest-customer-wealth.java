19bea1e9690c260924993928c85dd28b47119890
Seonjun Moon <70519480+thelovemsg@users.noreply.github.com>
2022-08-28 22:50:59 +0900

    Time: 0 ms (100.00%), Space: 43.1 MB (70.88%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.java
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -1,5 +1,17 @@
 class Solution {
     public int maximumWealth(int[][] accounts) {
-        return Arrays.stream(accounts).flatMapToInt(s -> IntStream.of(Arrays.stream(s).sum())).max().getAsInt();
+        //return Arrays.stream(accounts).flatMapToInt(s -> IntStream.of(Arrays.stream(s).sum())).max().getAsInt();
+        
+        int maxsum=0;
+        int n=accounts.length;
+        int m=accounts[0].length;
+        for(int i=0;i<n;i++){
+            int sum=0;
+            for(int j=0;j<m;j++){
+                sum+=accounts[i][j];
+            }
+            maxsum=Math.max(sum,maxsum);
+        }
+        return maxsum;
     }
 }
