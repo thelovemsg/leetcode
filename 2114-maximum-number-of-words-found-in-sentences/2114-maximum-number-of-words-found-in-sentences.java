45a106f1e35150a39708d3172f345b17e70ffdae
Seonjun Moon <70519480+thelovemsg@users.noreply.github.com>
2022-08-29 20:55:22 +0900

    Time: 6 ms (54.26%), Space: 45.3 MB (34.35%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.java
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.java
@@ -1,10 +1,11 @@
 class Solution {
     public int mostWordsFound(String[] sentences) {
-        int max = 0;
+        return Arrays.asList(sentences).stream().mapToInt(s -> s.split(" ").length).max().getAsInt();
+        /*int max = 0;
         for (String sentence : sentences) {
             String[] split = sentence.split(" ");
             max = Math.max(max,split.length);
         }
-        return max;
+        return max;*/
     }
 }
