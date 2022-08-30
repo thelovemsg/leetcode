d45935993c12b06c63d5f84ddc6cf1247fba3ca7
Seonjun Moon <70519480+thelovemsg@users.noreply.github.com>
2022-08-30 20:15:15 +0900

    Time: 1 ms (99.08%), Space: 43.1 MB (47.03%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
@@ -1,6 +1,6 @@
 class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
-        List<Boolean> results = new ArrayList<>();
+        /*List<Boolean> results = new ArrayList<>();
         for(int i=0; i<candies.length; i++){
             boolean flag = true;
             int target = candies[i];
@@ -18,6 +18,20 @@ class Solution {
             }
         }
         System.out.println(results);
-        return results;
+        return results;*/
+        
+        int maxCandies = candies[0];
+        for (int i = 1; i < candies.length;i++){
+            if (maxCandies < candies[i]){
+                maxCandies = candies[i];
+            }
+        }
+        
+        //모두 다 비교할 필요 없이 가장 큰 수보다 크거나 같기만 하면 된다. 
+        List<Boolean> arr = new ArrayList<>();
+        for (int i = 0; i < candies.length;i++){
+            arr.add(extraCandies + candies[i] >= maxCandies);
+        }
+        return arr;
     }
 }
