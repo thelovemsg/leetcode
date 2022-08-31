05ecf7b1232a2dbcf0d5b54f3340e75544f0ff1e
Seonjun Moon <70519480+thelovemsg@users.noreply.github.com>
2022-08-31 22:35:38 +0900

    Time: 15 ms (5.06%), Space: 49 MB (19.28%) - LeetHub

---
+++ 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] decompressRLElist(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        for(int i=0; i<nums.length; i+=2){
+            for(int j=0; j<nums[i]; j++){
+                list.add(nums[i+1]);
+            }
+        }
+        
+        return list.stream().mapToInt(Integer::intValue).toArray();
+        
+    }
+}
