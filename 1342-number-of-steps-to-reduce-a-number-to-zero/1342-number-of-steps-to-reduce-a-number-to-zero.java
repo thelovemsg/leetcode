cfbb71eeaba7488feb5c6652a9c9173f68c6de23
Seonjun Moon <70519480+thelovemsg@users.noreply.github.com>
2022-09-01 22:55:31 +0900

    Time: 1 ms (69.83%), Space: 40.7 MB (67.88%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.java
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.java
@@ -1,6 +1,6 @@
 class Solution {
     public int numberOfSteps(int num) {
-        int result = 0;
+        /*int result = 0;
         while(num >0){
             if(num%2==0){
                 num /= 2;
@@ -9,6 +9,13 @@ class Solution {
             }
             result++;
         }
-        return result;
+        return result;*/
+        
+        int c=0;
+        while(num!=0){
+            num=num%2==0?num/2:num-1;
+            c++;
+        }
+        return c;
     }
 }
