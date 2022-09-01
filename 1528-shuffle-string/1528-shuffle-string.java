0c022779b2ec332a402f31180f6faf4f600630aa
Seonjun Moon <70519480+thelovemsg@users.noreply.github.com>
2022-09-02 00:03:14 +0900

    Time: 2 ms (65.05%), Space: 44.5 MB (70.18%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.java
+++ 1528-shuffle-string/1528-shuffle-string.java
@@ -1,11 +1,11 @@
 class Solution {
     public String restoreString(String s, int[] indices) {
-        String[] result = new String[indices.length];
-        for(int i=0; i<s.length(); i++){
-            result[indices[i]] = String.valueOf(s.charAt(i));
+          char[] arr=new char[s.length()];
+        
+        for(int i=0;i<arr.length;i++){
+            arr[indices[i]]=s.charAt(i);
         }
-        return Arrays.stream(result).reduce((s1, s2) -> {
-            return s1 + s2;
-        }).get();
+        
+        return String.valueOf(arr);
     }
 }
