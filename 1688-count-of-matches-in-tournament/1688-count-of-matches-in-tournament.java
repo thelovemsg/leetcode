15e24779f0d61d67936c017a8378c35d7101e01e
Seonjun Moon <70519480+thelovemsg@users.noreply.github.com>
2022-09-07 23:03:10 +0900

    Time: 24 ms (51.50%), Space: 41.8 MB (5.27%) - LeetHub

---
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numberOfMatches(int n) {
+        int result = 0;
+        while(n!=1){
+            if(n%2 == 0){
+                n = n/2;
+                result += n;
+            }else{
+                n = n - ((n-1)/2);
+                result += n-1;
+            }
+
+            System.out.println("result :: " + result);
+        }
+        return result;
+    }
+}
