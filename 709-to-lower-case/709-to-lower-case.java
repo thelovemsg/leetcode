dc9ec894f9cfef859b95c609bf66b7e80e615dab
Seonjun Moon <70519480+thelovemsg@users.noreply.github.com>
2022-09-12 17:57:44 +0900

    Time: 13 ms (5.20%), Space: 43.1 MB (6.25%) - LeetHub

--- 709-to-lower-case/709-to-lower-case.java
+++ 709-to-lower-case/709-to-lower-case.java
@@ -1,19 +1,19 @@
-// class Solution {
-//     public String toLowerCase(String s) {
-//         String result = "";
-//         for(Character c : s.toCharArray()){
-//             if(c < 91 && c>64)
-//                 result += (char)(c+32)+"";
-//             else 
-//                 result += c+"";
-//         }
-        
-//         return result;
-//     }
-// }
-
 class Solution {
     public String toLowerCase(String s) {
-        return s.toLowerCase();
+        String result = "";
+        for(Character c : s.toCharArray()){
+            if(c < 91 && c>64)
+                result += (char)(c+32)+"";
+            else 
+                result += c+"";
+        }
+        
+        return result;
     }
 }
+
+// class Solution {
+//     public String toLowerCase(String s) {
+//         return s.toLowerCase();
+//     }
+// }
