2405fead96f5df69f8031b11753b5fd03cdfa7d8
Seonjun Moon <70519480+thelovemsg@users.noreply.github.com>
2022-09-12 14:23:59 +0900

    Time: 833 ms (39.63%), Space: 0B (100.00%) - LeetHub

--- 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
+++ 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
@@ -1,10 +1,15 @@
-# Write your MySQL query statement below
-SELECT user_id, time_stamp as last_stamp from Logins t1
-where t1.time_stamp 
-    = (SELECT 
-            MAX(time_stamp)
-         FROM Logins t2
-         WHERE t2.user_id = t1.user_id
-          and YEAR(t2.time_stamp) = 2020
-         GROUP BY t2.user_id
-        )
+# # Write your MySQL query statement below
+# SELECT user_id, time_stamp as last_stamp from Logins t1
+# where t1.time_stamp 
+#     = (SELECT 
+#             MAX(time_stamp)
+#          FROM Logins t2
+#          WHERE t2.user_id = t1.user_id
+#           and YEAR(t2.time_stamp) = 2020
+#          GROUP BY t2.user_id
+#         )
+        
+SELECT user_id , max(time_stamp) as last_stamp
+FROM Logins
+where  SUBSTR(time_stamp,1,4) = '2020'
+GROUP BY user_id
