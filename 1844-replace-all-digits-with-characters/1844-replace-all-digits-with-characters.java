1cf704108a2f3769c0ac62c5a53d533938c4ae49
Seonjun Moon <70519480+thelovemsg@users.noreply.github.com>
2022-09-16 14:06:38 +0900

    Time: 1 ms (82.35%), Space: 41.8 MB (80.16%) - LeetHub

--- 1844-replace-all-digits-with-characters/1844-replace-all-digits-with-characters.java
+++ 1844-replace-all-digits-with-characters/1844-replace-all-digits-with-characters.java
@@ -1,5 +1,7 @@
 class Solution {
     public String replaceDigits(String s) {
+        
+        /* 내 풀이... 맨 마지막 거를 빼먹어서 다시 확인하는 것 자체가 넌센스
         StringBuilder sb = new StringBuilder();
         for(int i=0; i<s.length()-1; i+=2){
             char alphabet = s.charAt(i);
@@ -15,7 +17,24 @@ class Solution {
                 sb.append(temp);
             }
         }
-
-        return sb.toString();
+        
+        //ㅠㅠ 이
+        StringBuilder sb = new StringBuilder();
+        for(int i=1; i<s.length(); i+=2){
+            char alphabet = s.charAt(i-1);
+            int number = (int)(s.charAt(i) -'0');
+            sb.append(alphabet);
+            sb.append((char)(alphabet+number));
+        }
+        return sb.toString(); 
+        */
+        
+        //애초에 새롭게 담을 필요가 없다. 기존에 있는 데이터를 조작해서 충분히 처리가 가능하기 때문이다 ㅠㅠ
+        char[] charArray = s.toCharArray();
+		for(int i = 1; i<charArray.length; i = i + 2) {
+		  charArray[i] = (char) (charArray[i - 1] + charArray[i] - '0');
+		}
+		return String.valueOf(charArray);       
+       
     }
 }
