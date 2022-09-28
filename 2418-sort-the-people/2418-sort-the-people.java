076a676ecd7ac224c68bc5ef48d0cd2339bb1281
Seonjun Moon <70519480+thelovemsg@users.noreply.github.com>
2022-09-28 22:00:06 +0900

    Time: 19 ms (45.37%), Space: 54.5 MB (63.97%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.java
+++ 2418-sort-the-people/2418-sort-the-people.java
@@ -1,7 +1,8 @@
 class Solution {
-    public String[] sortPeople(String[] names, int[] heights) {
+    public String[] sortPeople(String[] n, int[] h) {
      
-        for(int i=0; i<names.length; i++){
+        /*사실 그렇게 좋지 않은 방법.*/
+        /*for(int i=0; i<names.length; i++){
             for(int j=i+1; j<names.length; j++){
                 if(heights[i] > heights[j]){
                     String temp1 = names[j];
@@ -17,6 +18,25 @@ class Solution {
         
         List<String> result = Arrays.stream(names).collect(Collectors.toList());
         Collections.reverse(result);
-        return result.toArray(new String[result.size()]);
+        return result.toArray(new String[result.size()]);*/
+        
+        List<pair> A=new ArrayList<pair>();
+        for(int i=0;i<n.length;i++){
+            A.add(new pair(h[i],n[i]));
+        }
+        Collections.sort(A,(a,b)->b.first-a.first);
+        for(int i=0;i<n.length;i++){
+            n[i]=A.get(i).second;
+        }
+        return n;
+    }
+}
+
+class pair{
+    int first;
+    String second;
+    pair(int first,String second){
+        this.first=first;
+        this.second=second;
     }
 }
