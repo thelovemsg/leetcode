74b2ed1948b4263f8dccb80d3cf0234f3a0c82f5
Seonjun Moon <70519480+thelovemsg@users.noreply.github.com>
2023-12-16 16:25:25 +0900

    Time: 3 ms (89.36%), Space: 42.1 MB (78.74%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,18 +1,17 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         int[] sArr = new int[26];
-        int[] tArr = new int[26];
         
         for(int i=0; i<s.length(); i++){
             sArr[s.charAt(i) - 'a'] += 1;
         }
         
         for(int i=0; i<t.length(); i++){
-            tArr[t.charAt(i) - 'a'] += 1;
+            sArr[t.charAt(i) - 'a'] -= 1;
         }
     
         for(int i=0; i<sArr.length; i++) {
-            if(sArr[i] != tArr[i])
+            if(sArr[i] != 0)
                 return false;
         }
         
