a9889bd9d02c37b900acc6dffc5be79100a0159e
Seonjun Moon <70519480+thelovemsg@users.noreply.github.com>
2023-12-25 20:44:38 +0900

    Time: 1 ms (99.53%), Space: 46.8 MB (17.03%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -2,15 +2,7 @@ class Solution {
     public int[] countBits(int n) {
         int[] result = new int[n+1]; 
         for(int i=1; i<=n; i++) {
-            int target = i;
-            int cnt = 0;
-            while(target > 0){
-                if((target & 1) == 1)
-                    cnt++;
-                target = target>>1;
-            }
-            
-            result[i] = cnt;
+            result[i] = result[i/2] + i%2;
         }
         
         return result;
