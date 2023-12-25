92af4c6cc820b4391abcaec656698786f706c282
Seonjun Moon <70519480+thelovemsg@users.noreply.github.com>
2023-12-25 21:20:06 +0900

    Time: 1 ms (91.13%), Space: 42 MB (5.94%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -5,25 +5,12 @@ class Solution {
         temp.add(1);
         result.add(temp);
         
-        if(numRows == 1){
-            return result;
-        }
-        
-        List<Integer> temp2 = new ArrayList<>();        
-        temp2.add(1);
-        temp2.add(1);
-        result.add(temp2);
-        if(numRows == 2){
-            return result;
-        }
-        
-        for(int i=2; i<numRows; i++) {
+        for(int i=1; i<numRows; i++) {
             List<Integer> newList = new ArrayList<>();
             List<Integer> prevList = result.get(i-1);
             newList.add(1);
             for(int j=0; j<prevList.size()-1; j++) {
                 int newInt = prevList.get(j) + prevList.get(j+1);
-                System.out.println("newInt :: " + newInt);
                 newList.add(newInt);
             }
             newList.add(1);
