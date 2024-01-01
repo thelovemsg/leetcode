b47f2149906ff64d1d6216df1d43fc54ce59afd4
Seonjun Moon <70519480+thelovemsg@users.noreply.github.com>
2024-01-01 21:36:43 +0900

    Time: 12 ms (10.01%), Space: 44.9 MB (30.60%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        
+        int result = 0;
+        int i=0;
+        int target = 0;
+        int cnt =0;
+        while(i < g.length && cnt < s.length) {
+            if(s[cnt] >= g[i]) {
+                result++;
+                i++;
+            } 
+            cnt++;
+        }
+        
+        return result;
+    }
+}
