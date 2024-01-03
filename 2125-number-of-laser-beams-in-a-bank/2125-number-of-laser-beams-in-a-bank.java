1f7f48791ae8815bd1703a4388d841fe1226607e
Seonjun Moon <70519480+thelovemsg@users.noreply.github.com>
2024-01-03 17:30:20 +0900

    Time: 69 ms (6.15%), Space: 45.4 MB (8.53%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.java
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.java
@@ -1,42 +1,33 @@
 class Solution {
     public int numberOfBeams(String[] bank) {
-        int cnt = 0;
-        int i=0;
-        int j=1;
-        int length = bank.length;
-        while(i < length && j < length) {
-            if(j == i) {
-                j++;
-                continue;
-            }
-            
-            String prev = bank[i];
-            String curr = bank[j];
-            int prevCnt = 0;
-            int currCnt = 0;
-
-            for(int k=0; k<prev.length(); k++) {
-                if(prev.charAt(k) == '1')
-                    prevCnt++;
-                 if(curr.charAt(k) == '1')
-                    currCnt++;
-            }
-            
-            if(prevCnt == 0) {
-                i++;
-                continue;
-            }
-            
-            if(currCnt == 0) {
+        List<Long> collect = Arrays.stream(bank).map(s -> s.chars().filter(c -> c == '1').count())
+            .collect(Collectors.toList());
+        
+        int i = 0;
+        int j = 1;
+        int result = 0;
+        while(i < collect.size() && j < collect.size()) {
+            if(i == j)  {
                 j++;
                 continue;
             }
                 
-            cnt += prevCnt * currCnt;
+            int prev = collect.get(i).intValue();
+            if(prev == 0){
+                i++;
+                continue;
+            }
+                
+            int curr = collect.get(j).intValue();
+            if(curr == 0){
+                j++;
+                continue;
+            }
+            result += prev * curr;
             i++;
             j++;
         }
         
-        return cnt;
+        return result;
     }
 }
