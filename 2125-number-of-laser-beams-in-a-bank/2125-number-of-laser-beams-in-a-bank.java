6fbf721c983d374bb5c2aaaf74809fef9a3a2d97
Seonjun Moon <70519480+thelovemsg@users.noreply.github.com>
2024-01-03 17:14:56 +0900

    Time: 55 ms (6.75%), Space: 44.6 MB (40.67%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.java
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.java
@@ -5,37 +5,33 @@ class Solution {
         int j=1;
         int length = bank.length;
         while(i < length && j < length) {
-            if(i == j) {
+            if(j == i) {
                 j++;
                 continue;
             }
-                
             
             String prev = bank[i];
+            String curr = bank[j];
             int prevCnt = 0;
+            int currCnt = 0;
+
             for(int k=0; k<prev.length(); k++) {
                 if(prev.charAt(k) == '1')
                     prevCnt++;
+                 if(curr.charAt(k) == '1')
+                    currCnt++;
             }
             
             if(prevCnt == 0) {
                 i++;
                 continue;
             }
-                
-            String curr = bank[j];
-            int currCnt = 0;
-            for(int k=0; k<curr.length(); k++) {
-                if(curr.charAt(k) == '1')
-                    currCnt++;
-            }
             
             if(currCnt == 0) {
                 j++;
                 continue;
             }
-            
-            
+                
             cnt += prevCnt * currCnt;
             i++;
             j++;
