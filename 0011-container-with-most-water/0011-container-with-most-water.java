f797ec7a733c046a25713ec41c8d9ad1c4086687
Seonjun Moon <70519480+thelovemsg@users.noreply.github.com>
2024-01-06 14:28:37 +0900

    Time: 4 ms (87.33%), Space: 57.6 MB (22.34%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,9 +1,8 @@
 class Solution {
     public int maxArea(int[] height) {
-        int length = height.length;
         int left = 0;
         int result = 0;
-        int right = length - 1;
+        int right = height.length - 1;
         while(left < right) {
             int l = height[left];
             int r = height[right];
@@ -17,11 +16,7 @@ class Solution {
                 right--;
             }
             
-            // System.out.println("target :: " + target);
-            // System.out.println("diff :: " + diff);
             result = Math.max(result, target * diff);
-            // System.out.println("reuslt :: " + result);
-            // System.out.println("=================");           
         }
         
         
