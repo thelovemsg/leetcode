fd010fd8c70b36ae9e1c478ef622cf6087c3376d
Seonjun Moon <70519480+thelovemsg@users.noreply.github.com>
2024-01-14 16:41:28 +0900

    Time: 11 ms (78.03%), Space: 45.4 MB (34.32%) - LeetHub

---
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
@@ -0,0 +1,47 @@
+class Solution {
+    public boolean closeStrings(String word1, String word2) {
+        int length1 = word1.length();
+        int length2 = word2.length();
+        if(length1 < length2)
+            return false;
+        
+        int[] arr1 = new int[26];
+        int[] arr2 = new int[26];
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for(int i=0; i<length1; i++) {
+            arr1[word1.charAt(i)-'a']++;
+            arr2[word2.charAt(i)-'a']++;
+        }
+
+        for(int i=0; i<arr1.length; i++) {
+            if(arr1[i] != 0 && arr2[i] == 0)
+                return false;
+            
+            if(arr1[i] == 0 && arr2[i] != 0)
+                return false;
+            
+        }
+        
+        for(int i=0; i<arr1.length; i++) {
+            int target = arr1[i];
+            map.put(target, map.getOrDefault(target, 0) + 1);
+        }
+        
+        System.out.println("=====================");
+        
+        for(int i=0; i<arr2.length; i++) {
+            int target = arr2[i];
+            map.put(target, map.getOrDefault(target, 0) - 1);            
+        }
+        
+        for (Integer value : map.values()) {
+            if (value < 0) {
+                return false;
+            }
+        }
+
+        return true;
+        
+    }
+}
