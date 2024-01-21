7cd0d231171afcbba6a56664ac163b3c4f6dcc07
Seonjun Moon <70519480+thelovemsg@users.noreply.github.com>
2024-01-21 15:42:29 +0900

    Time: 1 ms (50.11%), Space: 42.7 MB (94.60%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,18 +1,13 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
         int length = cost.length;
-        int[] dp = new int[length + 1];
-
-        // 0번째와 1번째 계단까지의 비용은 0 (시작점)
-        dp[0] = 0;
-        dp[1] = 0;
-
-        for (int i = 2; i <= length; i++) {
-            // i번째 계단에 도달하기 위한 최소 비용 계산
-            dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+        int[] dp = new int[length];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for (int i = 2; i < length; i++) {
+            dp[i] = Math.min(dp[i-1], dp[i-2]) + cost[i];
         }
-
-        // 계단 꼭대기에 도달하는 데 필요한 최소 비용 반환
-        return dp[length];
+        
+        return Math.min(dp[length-1], dp[length-2]);
     }
 }
