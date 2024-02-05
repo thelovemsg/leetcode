70baf1e97f709abcb72149dd4351ffd0cb296a6c
Seonjun Moon <70519480+thelovemsg@users.noreply.github.com>
2024-02-05 22:26:37 +0900

    Time: 11 ms (86.57%), Space: 44.7 MB (87.09%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -0,0 +1,57 @@
+class Solution {
+    public String frequencySort(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for(int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            map.merge(c, 1, Integer::sum);
+        }
+        
+        List<Bunch> list = new ArrayList<>();
+        
+        for(char c : map.keySet()) {
+            list.add(new Bunch(c,map.get(c)));
+        }
+        
+        Collections.sort(list);
+        
+        StringBuilder sb = new StringBuilder();
+        for(Bunch b : list) {
+            int cnt = b.getCnt();
+            char c = b.getC();
+            for(int i=0; i<cnt; i++) {
+                sb.append(b.getC());
+            }
+        }
+        
+        return sb.toString();
+        
+    }
+    
+    public static class Bunch implements Comparable<Bunch>{
+        private char c;
+        private int cnt;
+        public Bunch(char c, int cnt) {
+            this.c = c;
+            this.cnt = cnt;
+        }
+        
+        private String print() {
+            return "c :: " + c + " / cnt : " + cnt;
+        }
+        
+        public int getCnt() {
+            return cnt;
+        }
+        
+        public char getC() {
+            return c;
+        }
+        
+        
+        @Override
+        public int compareTo(Bunch b) {
+            return b.getCnt() - this.cnt;
+        }
+    }
+    
+}
