b52afdd6153df091d7e14aec08d7b0cd9712955c
Seonjun Moon <70519480+thelovemsg@users.noreply.github.com>
2024-02-06 23:06:55 +0900

    Time: 9 ms (34.75%), Space: 48.4 MB (11.94%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String getString(String s) {
+        int[] count = new int[26];
+        for (char c : s.toCharArray()) {
+            count[c - 'a']++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) {
+                sb.append((char) ('a' + i)).append(count[i]);
+            }
+        }
+        return sb.toString();
+    }
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> result = new ArrayList<>();
+        Map<String, List<String>> groups = new HashMap<>();
+
+        for (String s : strs) {
+            groups.computeIfAbsent(getString(s), k -> new ArrayList<>()).add(s);
+        }
+
+        result.addAll(groups.values());
+
+        return result;
+    }
+}
