f9c18feada8bd834ee8fcc06e51f37bcc53ef804
Seonjun Moon <70519480+thelovemsg@users.noreply.github.com>
2024-02-16 22:57:36 +0900

    Time: 792 ms (5.09%), Space: 62.6 MB (25.68%) - LeetHub

---
+++ 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.java
@@ -0,0 +1,65 @@
+class Solution {
+    public int findLeastNumOfUniqueInts(int[] arr, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(Integer num : arr) {
+            map.merge(num, 1, Integer::sum);
+        }
+        
+        List<KeyValue> list = new ArrayList<>();
+        
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            list.add(new KeyValue(entry.getKey(), entry.getValue()));
+        }
+                     
+        Collections.sort(list);
+        for(int i=0; i<list.size(); i++) {
+            KeyValue kv = list.get(i);
+            System.out.println(kv.getValue());
+        }
+                     
+        for(KeyValue kv : list) {
+            int value = kv.getValue();
+            while(k > 0 && value > 0){
+                k--;
+                kv.setValue(--value);
+            }
+            
+            if(k == 0)
+                break;
+        }
+        
+        for(int i=0; i<list.size(); i++) {
+            KeyValue kv = list.get(i);
+            System.out.println(kv.getValue());
+        }
+        
+        return (int) list.stream().filter(el -> el.getValue()!=0).count();
+        
+    }
+    
+    public static class KeyValue implements Comparable<KeyValue> {
+        private Integer key;
+        private Integer value;
+    
+        public KeyValue(Integer key, Integer value) {
+            this.key = key;
+            this.value = value;
+        }
+        
+        public int compareTo(KeyValue kv) {
+            return this.getValue() - kv.getValue();
+        }
+        
+        public Integer getValue() {
+            return value;
+        }
+        
+        public Integer getKey() {
+            return key;
+        }
+        
+        public void setValue(Integer value) {
+            this.value = value;
+        }
+    }
+}
