88b4493836be1c345fc323c47eba6cde7bae9dcd
Seonjun Moon <70519480+thelovemsg@users.noreply.github.com>
2024-03-02 00:23:47 +0900

    Time: 2 ms (83.47%), Space: 43 MB (85.23%) - LeetHub

--- 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.java
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.java
@@ -1,48 +1,25 @@
 class Solution {
     public String maximumOddBinaryNumber(String s) {
-        int cnt = 0;
-        for(char c : s.toCharArray()) {
-            if(c == '1') 
-                cnt++;
+        int oneCount = 0; // '1'의 총 개수를 세기 위한 변수입니다.
+        // 문자열 s에서 '1'의 총 개수를 세는 반복문입니다.
+        for (char c : s.toCharArray()) {
+            if (c == '1') {
+                oneCount++;
+            }
         }
 
-        int length = s.length();
         StringBuilder sb = new StringBuilder();
-            
-        if(cnt == 0){
-            for(int i=0; i<length; i++) {
-                 sb.append("0");
-            }
-            
-            return sb.toString();
-        }
-            
-        if(cnt == 1) {
-            for(int i=0; i<length-1; i++) {
-                 sb.append("0");
-            }
-            
+        // '1'을 oneCount-1 만큼 앞으로 추가합니다. 마지막 '1'은 홀수를 만들기 위해 맨 뒤에 위치시킬 것입니다.
+        for (int i = 0; i < oneCount - 1; i++) {
             sb.append("1");
-            
-            return sb.toString();
         }
-        
-        cnt--;
-            
-        for(int i=0; i<length-1; i++) {
-            if(cnt == 0){
-                sb.append("0");
-            } else if(cnt > 0 ){
-                cnt--;
-                sb.append("1");
-            }
-                
-                
+        // 나머지 자리에 '0'을 채웁니다. 마지막에 '1'을 추가하기 전까지입니다.
+        while (sb.length() < s.length() - 1) {
+            sb.append("0");
         }
-
+        // 마지막에 '1'을 추가하여 홀수 이진 숫자를 완성합니다.
         sb.append("1");
 
         return sb.toString();
-        
     }
 }
