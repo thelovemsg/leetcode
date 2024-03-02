60e7e49f8ed4009c79fdf7782f86629ee8f082c6
Seonjun Moon <70519480+thelovemsg@users.noreply.github.com>
2024-03-02 14:07:40 +0900

    Time: 8 ms (18.06%), Space: 47 MB (61.58%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -1,12 +1,10 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        List<Integer> list = new ArrayList<>();
-        for(int num : nums) {
-            list.add(num*num);
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = nums[i] * nums[i]; // 각 요소를 제곱
         }
-        
-        Collections.sort(list);
-        
-        return list.stream().mapToInt(Integer::intValue).toArray();
+
+        Arrays.sort(nums); // 배열 정렬
+        return nums; // 정렬된 배열 반환
     }
 }
