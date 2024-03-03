13bd8c58f3673bdb8b75ba3d84a7e8bec5d714bd
Seonjun Moon <70519480+thelovemsg@users.noreply.github.com>
2024-03-03 17:29:48 +0900

    Time: 0 ms (100.00%), Space: 41.5 MB (62.87%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -8,29 +8,27 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
+public class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        int len = 0;
-        ListNode temp = head;
-        
-        while(temp != null) {
-            len++;
-            temp = temp.next;
+        ListNode dummy = new ListNode(0); // 더미 노드 생성
+        dummy.next = head;
+        ListNode fast = dummy;
+        ListNode slow = dummy;
+
+        // Fast 포인터를 n+1만큼 이동시켜 놓음
+        for (int i = 0; i <= n; i++) {
+            fast = fast.next;
         }
-        
-        temp = head;
-        ListNode prev = null;
-        int pos = len - n;
-        
-        if(pos == 0) return head.next;
-        
-        for(int i = 0; i < pos; i++) {
-            prev = temp;
-            temp = temp.next;
+
+        // Fast가 리스트의 끝에 도달할 때까지 Fast와 Slow를 함께 이동
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
         }
-        
-        prev.next = temp.next;
-        
-        return head;
+
+        // Slow의 다음 노드를 삭제
+        slow.next = slow.next.next;
+
+        return dummy.next; // 더미 노드의 다음 노드 반환
     }
 }
