064097c132c65242522feedb61081af5435cb248
Seonjun Moon <70519480+thelovemsg@users.noreply.github.com>
2024-03-06 23:29:22 +0900

    Time: 1 ms (25.90%), Space: 44.2 MB (66.74%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -21,12 +21,6 @@ public class Solution {
         if(first == second)
             return true;
         
-        if(first.next == null)
-            return false;
-        
-        if(second.next == null)
-            return false;
-        
         while(first != null && second != null) {
             first = first.next;
             if(first == null)
