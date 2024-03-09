d4a1baed76bbb3c98c254c3774fd8e3364b2d026
Seonjun Moon <70519480+thelovemsg@users.noreply.github.com>
2024-03-09 15:19:52 +0900

    Time: 1 ms (96.67%), Space: 62.2 MB (88.00%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.java
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -8,8 +8,6 @@ class Solution {
         int idx2 = 0;
             
         while(idx1 < length1 && idx2 < length2) {
-            System.out.println("idx1 :: " + idx1);
-            System.out.println("idx2 :: " + idx2);
             int num1 = nums1[idx1];
             int num2 = nums2[idx2];
  
