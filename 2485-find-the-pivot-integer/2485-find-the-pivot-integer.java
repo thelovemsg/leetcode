3d6b31a71e2f8b366bc88717f184eb3883481f4c
Seonjun Moon <70519480+thelovemsg@users.noreply.github.com>
2024-03-13 23:30:52 +0900

    Time: 1 ms (77.54%), Space: 40.7 MB (43.66%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
@@ -4,19 +4,12 @@ class Solution {
         int tail = n;
         
         for(int i=1; i<=n; i++) {
-            int stand = i;
-            int front = 0;
-            int back = 0;
-            for(int j = 1; j<=i; j++) {
-                front+=j;
-            }
+            int result1 = i*(i+1)/2;
+            int result2 = n*(n+1)/2 - result1 + i ;
             
-            for(int j = i; j<=n; j++) {
-                back+=j;
-            }
-            
-            if( back == front)
+            if(result1 == result2)
                 return i;
+            
         }
             
         return -1;
